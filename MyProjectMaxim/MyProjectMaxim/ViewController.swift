e05d0365544a8e8cb0bf9fcffd45a7e2f6dcf86e
seymondor <112369366+seymondor@users.noreply.github.com>
2023-11-04 17:10:48 +1000

    Add files via upload

---
+++ MyProjectMaxim/MyProjectMaxim/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MyProjectMaxim
+//
+//  Created by 1 on 14.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var Enter2: UITextField!
+    @IBOutlet weak var Enter1: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+

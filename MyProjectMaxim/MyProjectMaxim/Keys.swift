6d60945d611a45681a460cb49b02271449e01957
SergeyWolf <MackPro1970!>
2023-11-04 18:27:45 +1000

    no message

--- MyProjectMaxim/MyProjectMaxim/Keys.swift
+++ MyProjectMaxim/MyProjectMaxim/Keys.swift
@@ -4,5 +4,65 @@
 //
 //  Created by 1 on 04.11.2023.
 //
-
 import Foundation
+final class Keys {
+    private enum HumanSettings: String{
+        case height
+        case weight
+        case age
+        case gender
+    }
+    static var height: String! {
+        get {
+            return UserDefaults.standard.string(forKey: HumanSettings.height.rawValue)
+        } set {
+            let defaults = UserDefaults.standard
+            let key = HumanSettings.height.rawValue
+            if let kladr = newValue {
+                defaults.set(kladr, forKey: key)
+            }
+        }
+        
+    }
+    
+    static var weight: String! {
+        get {
+            return UserDefaults.standard.string(forKey: HumanSettings.weight.rawValue)
+        } set {
+            let defults = UserDefaults.standard
+            let key = HumanSettings.weight.rawValue
+            if let kladr = newValue {
+                defults.set(kladr, forKey: key)
+            }
+        }
+        
+    }
+    
+    static var age: String! {
+        get {
+            return UserDefaults.standard.string(forKey: HumanSettings.age.rawValue)
+        } set {
+            let defaults = UserDefaults.standard
+            let key = HumanSettings.age.rawValue
+            if let kladr = newValue {
+                defaults.set(kladr, forKey: key)
+            }
+        }
+        
+    }
+    
+    static var gender: String! {
+        get {
+            return UserDefaults.standard.string(forKey: HumanSettings.gender.rawValue)
+        } set {
+            let defaults = UserDefaults.standard
+            let key = HumanSettings.gender.rawValue
+            if let kladr = newValue {
+                defaults.set(kladr, forKey: key)
+            }
+        }
+        
+    }
+    
+    
+}
--- MyProjectMaxim/MyProjectMaxim/ViewController.swift
+++ MyProjectMaxim/MyProjectMaxim/ViewController.swift
@@ -11,8 +11,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var Enter2: UITextField!
     @IBOutlet weak var Enter1: UITextField!
     @IBOutlet weak var Enter3: UITextField!
+    @IBAction func StartButton(_ sender: UIButton) {
+        var Text1 = Enter1.text
+        Test.text = Text1
+        Test.isHidden = false
+    }
+    @IBOutlet weak var Test: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        Test.isHidden = true
+        Enter1.textAlignment = .center
+        Enter2.textAlignment = .center
+        Enter3.textAlignment = .center
     }
 }
 

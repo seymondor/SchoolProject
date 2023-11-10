66d26a1ddec61e0283edac09bc80fcc5c612f366
Seymondor <maxm22@bk.ru>
2023-11-10 21:21:33 +1000

    no message

--- MyProjectMaxim/MyProjectMaxim/ViewController.swift
+++ MyProjectMaxim/MyProjectMaxim/ViewController.swift
@@ -8,6 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var Label1: UILabel!
+    @IBOutlet weak var Label2: UILabel!
+    @IBOutlet weak var Label3: UILabel!
+    @IBOutlet weak var Label4: UILabel!
+    
+    func checkButtonManOrWoman(buttonMan:UIButton,buttonWoman:UIButton) -> String {
+        if buttonMan.layer.borderColor == #colorLiteral(red: 0.3026678778, green: 0.6489531224, blue: 0.9067135847, alpha: 1) {
+            return "Man"
+        } else if buttonWoman.layer.borderColor == #colorLiteral(red: 0.3026678778, green: 0.6489531224, blue: 0.9067135847, alpha: 1) {
+            return "Woman"
+        } else {
+            return "Error"
+        }
+    }
     func createFrameForButton(button: UIButton, button2: UIButton){
         if button2.layer.borderColor == #colorLiteral(red: 0.3026678778, green: 0.6489531224, blue: 0.9067135847, alpha: 1) {
             button2.layer.borderColor = #colorLiteral(red: 0.4778209925, green: 0.7600174546, blue: 0.8799687028, alpha: 1)
@@ -30,23 +44,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var StartEnterHeight: UITextField!
     @IBOutlet weak var StartEnterAge: UITextField!
     @IBAction func StartButton(_ sender: UIButton) {
-        Keys.height = StartEnterHeight.text
-        Keys.weight = StartEnterWeight.text
-        Keys.age = StartEnterAge.text
+        if StartEnterHeight.text == "" {
+            Keys.height = "Error"
+        } else if let height = StartEnterHeight.text {
+            Keys.height = height
+        }
+        if StartEnterWeight.text == "" {
+            Keys.weight = "Error"
+        } else if let weight = StartEnterWeight.text {
+            Keys.weight = weight
+        }
+        if StartEnterAge.text == "" {
+            Keys.age = "Error"
+        } else if let age = StartEnterAge.text {
+            Keys.age = age
+        }
+        Keys.gender = checkButtonManOrWoman(buttonMan: OutletManButton, buttonWoman: OutletWomanButton)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        StartEnterHeight.textAlignment = .center; StartEnterHeight.layer.cornerRadius = 5;
-            StartEnterHeight.layer.borderWidth = 1;
-            StartEnterHeight.layer.borderColor = #colorLiteral(red: 0.4778209925, green: 0.7600174546, blue: 0.8799687028, alpha: 1)
-        StartEnterWeight.textAlignment = .center ;
-            StartEnterWeight.layer.cornerRadius = 5;
-            StartEnterWeight.layer.borderWidth = 1;
-            StartEnterWeight.layer.borderColor = #colorLiteral(red: 0.4778209925, green: 0.7600174546, blue: 0.8799687028, alpha: 1)
-        StartEnterAge.textAlignment = .center ;
-            StartEnterAge.layer.cornerRadius = 5;
-            StartEnterAge.layer.borderWidth = 1;
-            StartEnterAge.layer.borderColor = #colorLiteral(red: 0.4778209925, green: 0.7600174546, blue: 0.8799687028, alpha: 1)
+        StartEnterHeight.textAlignment = .center
+        StartEnterWeight.textAlignment = .center
+        StartEnterAge.textAlignment = .center
+        Label1.text = Keys.height
+        Label2.text = Keys.weight
+        Label3.text = Keys.age
+        Label4.text = Keys.gender
     }
 }
 

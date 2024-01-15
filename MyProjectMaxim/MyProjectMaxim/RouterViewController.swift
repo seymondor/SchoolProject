31450123a2ab4ff7692ef818ac018d057b5e68b9
Seymondor <maxm22@bk.ru>
2024-01-15 21:33:29 +1000

    router

--- MyProjectMaxim/MyProjectMaxim/RouterViewController.swift
+++ MyProjectMaxim/MyProjectMaxim/RouterViewController.swift
@@ -10,8 +10,25 @@ import UIKit
 class RouterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    override func viewDidAppear(_ animated: Bool) {
         if Keys.age != "Error" && Keys.age != nil && Keys.gender != "Error" && Keys.gender != nil && Keys.height != "Error" && Keys.height != nil && Keys.weight != "Error" && Keys.weight != nil {
             // доделать роутер чтобы показывался главный экран
+            let vc = self.storyboard!.instantiateViewController(withIdentifier: "Home")
+            vc.modalPresentationStyle = .fullScreen
+            self.present(vc, animated: false, completion: nil)
+        } else {
+            let vc = self.storyboard!.instantiateViewController(withIdentifier: "Start")
+            vc.modalPresentationStyle = .fullScreen
+            self.present(vc, animated: false, completion: nil)
         }
     }
+    func resetDefaults() {
+        let defaults = UserDefaults.standard
+        let dictionary = defaults.dictionaryRepresentation()
+        dictionary.keys.forEach { key in
+            defaults.removeObject(forKey: key)
+        }
+    }
+
 }
